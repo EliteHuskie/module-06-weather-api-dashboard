558048437d055192f596b5631ada8d99281f5fd7
Jared Stratton <Stratton_10@hotmail.com>
2023-06-14 12:27:23 -0300

    Adding more logic to javascript file for weather dashboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,31 @@ searchForm.addEventListener('submit', function(event) {
 /* Function for Searching of City */
 function searchCity(city) {
   const geocodingApiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
+  /* Fetching for Api URL for Geocoding */
+  fetch(geocodingApiUrl)
+    .then(response => response.json())
+    .then(data => {
+      /* If statement for data.length and variables */
+      if (data.length > 0) {
+        const cityName = data[0].name;
+        const latitude = data[0].lat;
+        const longitude = data[0].lon;
+        const currentConditionsUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&units=metric&appid=${apiKey}`;
+        const fiveDayForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+        /* Responses when performing fetch for current conditions and five day forecast */
+        const currentConditionsPromise = fetch(currentConditionsUrl).then(response => response.json());
+        const fiveDayForecastPromise = fetch(fiveDayForecastUrl).then(response => response.json());
+
+        return Promise.all([currentConditionsPromise, fiveDayForecastPromise, cityName]);
+      } else {
+        throw new Error('City not found');
+      }
+    })
+    /* Console loggging for Current Conditions + Five Day Forecast */
+    .then(([currentConditionsData, fiveDayForecastData, cityName]) => {
+      console.log('Current Conditions Data:', currentConditionsData);
+      console.log('Five-Day Forecast Data:', fiveDayForecastData);
+    /* Error conditions */
+      if (!currentConditionsData || !currentConditionsData.main || !currentConditionsData.main.temp || !currentConditionsData.weather || !currentConditionsData.weather[0]) {
+        throw new Error('Invalid current conditions data');
+      }
